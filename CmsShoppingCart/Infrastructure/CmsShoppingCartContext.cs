3740e87ff688f50a8f0c597fb42fe24c5b465d6f
Dimitrii Vinogradov <dimitrii.vinogradov@timelysoft.net>
2021-10-14 17:21:19 +0600

    connected db, seeded data

---
+++ CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,15 @@
+﻿using CmsShoppingCart.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext([NotNull] DbContextOptions<CmsShoppingCartContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
--- CmsShoppingCart/Startup.cs
+++ CmsShoppingCart/Startup.cs
@@ -1,6 +1,8 @@
+using CmsShoppingCart.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,9 @@ namespace CmsShoppingCart
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<CmsShoppingCartContext>(options => options.UseSqlServer
+            (Configuration.GetConnectionString("CmsShoppingCartContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
