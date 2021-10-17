7ade2a161bc26181696318c3aa3ba21a011c7635
Dimitrii Vinogradov <siteddv@gmail.com>
2021-10-18 00:06:07 +0600

    implemented deleting for page and animations

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -55,7 +55,7 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             
             if(slug != null)
             {
-                ModelState.AddModelError("", "The title already exists.");
+                ModelState.AddModelError("", "The page already exists.");
                 return View(page);
             }
 
@@ -63,6 +63,64 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             await _context.SaveChangesAsync();
 
             TempData["Success"] = "The page has been added!";
+
+            return RedirectToAction("Index");
+        }
+
+        // GET /admin/pages/details/id
+        public async Task<IActionResult> Edit(int id)
+        {
+            var page = await _context.Pages.FindAsync(id);
+
+            if (page == null)
+                return NotFound();
+
+            return View(page);
+        }
+
+        // POST /admin/pages/create
+        [HttpPost]
+        public async Task<IActionResult> Edit(Page page)
+        {
+            if (!ModelState.IsValid)
+                return View(page);
+
+            page.Slug = page.Id == 1 
+                ? page.Slug 
+                : page.Title.ToLower().Replace(" ", "-");
+
+            var slug = await _context.Pages.FirstOrDefaultAsync(x => x.Id != page.Id && x.Slug == page.Slug);
+
+            if (slug != null)
+            {
+                ModelState.AddModelError("", "The page already exists.");
+                return View(page);
+            }
+
+            _context.Update(page);
+            await _context.SaveChangesAsync();
+
+            TempData["Success"] = "The page has been edited!";
+
+            return RedirectToAction("Edit", new { Id = page.Id });
+        }
+
+        // GET /admin/pages/delete/id
+        public async Task<IActionResult> Delete(int id)
+        {
+            var page = await _context.Pages.FindAsync(id);
+
+            if (page == null)
+            {
+                TempData["Error"] = "The page does not exist!";
+            }
+            else
+            {
+                _context.Pages.Remove(page);
+                await _context.SaveChangesAsync();
+
+                TempData["Success"] = "The page has been deleted!";
+            }
 
             return RedirectToAction("Index");
         }
--- CmsShoppingCart/wwwroot/js/site.js
+++ CmsShoppingCart/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
 
-// Write your JavaScript code.
+    if ($("a.confirmDeletion").length) {
+        $("a.confirmDeletion").click( () => {
+            if (!confirm("Do you realy want to delete this page?")) return false;
+        });
+    }
+
+    if ($("div.alert.notification").length) {
+        setTimeout( () => {
+            $("div.alert.notification").fadeOut();
+        }, 2000);
+    }
+});
