ad742a8e71d838e73f987ab98efc6160e06ffaf4
Dimitrii Vinogradov <siteddv@gmail.com>
2021-10-19 00:38:57 +0600

    added initial product files

---
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly CmsShoppingCartContext _context;
+
+        public ProductsController(CmsShoppingCartContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
