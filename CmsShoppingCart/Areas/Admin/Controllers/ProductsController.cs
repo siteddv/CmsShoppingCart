df09fa58ea952751b30c71264a34a225ade3c6a7
Dimitrii Vinogradov <siteddv@gmail.com>
2021-10-20 00:37:11 +0600

    implemented product uploading

--- CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -1,10 +1,11 @@
 ﻿using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Model;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CmsShoppingCart.Areas.Admin.Controllers
@@ -13,6 +14,7 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
     public class ProductsController : Controller
     {
         private readonly CmsShoppingCartContext _context;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
         public ProductsController(CmsShoppingCartContext context)
         {
@@ -35,6 +37,43 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             ViewBag.CategoryId = new SelectList(categories, "Id", "Name");
 
             return View();
-        } 
+        }
+
+        // POST /admin/pages/create
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+
+            product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+            var slug = await _context.Products.FirstOrDefaultAsync(x => x.Slug == product.Slug);
+
+            if (slug != null)
+            {
+                ModelState.AddModelError("", "The product already exists.");
+                return View(product);
+            }
+
+            var imageName = "noimage.png";
+
+            if(product.ImageUpload != null)
+            {
+                var uploadDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                imageName = $"{product.ImageUpload.FileName}_{Guid.NewGuid().ToString()}";
+                var filePath = Path.Combine(uploadDir, imageName);
+
+                using (var fs = new FileStream(filePath, FileMode.Create))
+                    await product.ImageUpload.CopyToAsync(fs);
+            }
+
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+
+            TempData["Success"] = "The product has been added!";
+
+            return RedirectToAction("Index");
+        }
     }
 }
