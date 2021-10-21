ef1f4e8abe761e5ada31c44f32ca8fcb1f86b5b4
Dimitrii Vinogradov <siteddv@gmail.com>
2021-10-21 23:16:13 +0600

    added products controller

---
+++ CmsShoppingCart/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly CmsShoppingCartContext _context;
+
+        public ProductsController(CmsShoppingCartContext context)
+        {
+            _context = context;
+        }
+
+        // GET /products
+        public async Task<IActionResult> Index(int p = 1)
+        {
+            var pageSize = 6;
+
+            var products = await _context.Products
+                .Skip((p - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.Products.Count() / pageSize);
+
+            return View(products);
+        }
+    }
+}
