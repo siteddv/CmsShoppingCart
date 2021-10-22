df011e0e943cc15db906464fad93b657d740c849
Dimitrii Vinogradov <dimitrii.vinogradov@timelysoft.net>
2021-10-22 14:00:34 +0600

    added CartController and SessionExtensions

---
+++ CmsShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CmsShoppingCartContext _context;
+
+        public CartController(CmsShoppingCartContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CmsShoppingCart/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public static class SessionExtensions
+    {
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+        
+        public static T GetJson<T>(this ISession session, string key)
+        {
+            var sessionData = session.GetString(key);
+
+            if (sessionData == null)
+                return default;
+
+            var result = JsonConvert.DeserializeObject<T>(sessionData);
+
+            return result;
+        }
+    }
+}
