f190ea29e9236463be45ece2cfd562caa6d34f48
Dimitrii Vinogradov <dimitrii.vinogradov@timelysoft.net>
2021-10-22 15:11:18 +0600

    added CartItem

---
+++ CmsShoppingCart/Model/CartItem.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace CmsShoppingCart.Model
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get { return Quantity * Price; } }
+        public string Image { get; set; }
+
+        public CartItem(Product product)
+        {
+            ProductId = product.Id;
+            ProductName = product.Name;
+            Price = product.Price;
+            Quantity = 1;
+            Image = product.Image;
+        }
+    }
+}
